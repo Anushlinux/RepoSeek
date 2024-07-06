145dc32741353c085aa8943e6dea6c0fba58b090
Anushlinux <anushrutpandit@gmail.com>
2024-07-06 22:53:19 +0530

    placeholder

--- reposeek/src/components/landingPage.jsx
+++ reposeek/src/components/landingPage.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import React from "react";
 import { AuroraBackground } from "../components/aurora-bg";
 import { FlipWords } from "../components/flip-words";
+import { Placeholder } from "../components/placeholder";
 
 import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react'
 
@@ -31,6 +32,20 @@ function classNames(...classes) {
 export default function Example() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
   const words = ["website", "lib", "model", "service", "app", "api", "server", "client", "database", "framework", "library", "tool", "platform", "system", "interface", "network", "protocol", "algorithm", "data", "object", "function", "variable", "constant", "class", "module",];
+  const placeholders = [
+    "What's the first rule of Fight Club?",
+    "Who is Tyler Durden?",
+    "Where is Andrew Laeddis Hiding?",
+    "Write a Javascript method to reverse a string",
+    "How to assemble your own PC?",
+  ];
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
 
   return (
     <div>
@@ -210,42 +225,54 @@ export default function Example() {
           </a>
         </div>
 
-        <main>
-          <div>
-            <AuroraBackground>
-              <motion.div
-                initial={{ opacity: 0.0, y: 40 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{
-                  delay: 0.3,
-                  duration: 0.8,
-                  ease: "easeInOut",
-                }}
-                className="relative flex flex-col gap-4 justify-items-start align-baseline px-10"
-                style={{ height: "81vh", marginRight: "20vw" }}
-              >
-                <div className="">
-                  <div className="text-lg  md:text-7xl font-semibold dark:text-white text-start">
-                    Hello, user
-                  </div>
-                  <div
-                    className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4"
+        <main className="min-h-screen relative lg:pl-72">
+          <AuroraBackground className="absolute inset-0 z-0" />
+          <div className="relative z-10 flex flex-col min-h-screen justify-between px-20">
+            <motion.div
+              initial={{ opacity: 0.0, y: 40 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{
+                delay: 0.3,
+                duration: 0.8,
+                ease: "easeInOut",
+              }}
+              className="flex flex-col gap-4 pt-20"
+            >
+              <div>
+                <div className="text-lg sm:text-5xl md:text-7xl font-semibold dark:text-white text-start">
+                  Hello,
+                  <span
                     style={{
-                      background: "linear-gradient(to right, grey, red)",
+                      background: "linear-gradient(to right, grey, yellow)",
                       WebkitBackgroundClip: "text",
                       WebkitTextFillColor: "transparent",
                     }}
                   >
-                    what you cookin today?
-                  </div>
-                  <FlipWords
-                    className="font-extralight text-base md:text-4xl dark:text-neutral-200 -px-1 -py-4"
-                    words={words}
-                  />
-                  <br />
+                    user
+                  </span>
                 </div>
-              </motion.div>
-            </AuroraBackground>
+                <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+                  what you
+                  <span className="font-serif"> cookin </span>
+                  today?
+                </div>
+                <FlipWords
+                  className="font-extralight text-base md:text-4xl dark:text-neutral-200"
+                  words={words}
+                />
+              </div>
+            </motion.div>
+
+            <div className="flex-grow flex mt-40 flex-col justify-center items-center">
+              {/* <h2 className="mb-10 text-xl text-center sm:text-5xl dark:text-white text-black">
+                Ask Aceternity UI Anything
+              </h2> */}
+              <Placeholder
+                placeholders={placeholders}
+                onChange={handleChange}
+                onSubmit={onSubmit}
+              />
+            </div>
           </div>
         </main>
       </div>
