a1e1cb4c4af8dbe40a78b88f4f57d0b412c44fac
Anushlinux <anushrutpandit@gmail.com>
2024-07-09 01:56:36 +0530

    callback (not complete)

--- reposeek/src/pages/Callback.jsx
+++ reposeek/src/pages/Callback.jsx
@@ -3,12 +3,25 @@ import React from 'react'
 
 
 const Callback = () => {
-    
+
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get('code');
     
     if (code) {
-        console.log(code);
+        fetch('http://localhost:5173/api/github/callback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ code }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('User data', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
     }
 
   return (
