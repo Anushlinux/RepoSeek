22cdbc8f8bea98fdb126c0d1df4dc7844b9dc80e
Anushlinux <anushrutpandit@gmail.com>
2024-07-09 16:34:45 +0530

    Reapply "incomplete shit"
    This reverts commit c9993fd7b58ffdb36e64a8aaa575b992a0581296.

--- reposeek/src/pages/Callback.jsx
+++ reposeek/src/pages/Callback.jsx
@@ -1,37 +1,21 @@
 // import React, { useEffect, useState } from "react";
 
-// const Callback = () => {
+const Callback = () => {
     
-//     const urlParams = new URLSearchParams(window.location.search);
-//     const code = urlParams.get("code");
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
 
-//     if (code) {
-      
-//       fetch("/api/github/callback", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ code }),
-//       })
-//         .then((response) => response.text())
-//         .then((data) => {
-//           // Handle the user data received from your backend
-//           console.log("User data:", data);
-//           // Update your UI or store the user info as needed
-//         })
-//         .catch((error) => {
-//           console.error("Error:", error);
-//         });
-//     }
+    if (code) {
+        console.log(code);
+    }
 
-//   return (
-//     <div>
-//         {code}
-//     </div>
-//   )
-// }
+  return (
+    <div>
+        {code}
+    </div>
+  )
+}
 
  
 
-// export default Callback;
+export default Callback;
