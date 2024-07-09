9712fce88dc71978359685369c0f92af49d32977
Anushlinux <anushrutpandit@gmail.com>
2024-07-09 16:26:02 +0530

    new changes

--- reposeek/src/pages/Callback.jsx
+++ reposeek/src/pages/Callback.jsx
@@ -1,21 +1,37 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-const Callback = () => {
+// const Callback = () => {
     
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get("code");
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const code = urlParams.get("code");
 
-    if (code) {
-        console.log(code);
-    }
+//     if (code) {
+      
+//       fetch("/api/github/callback", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ code }),
+//       })
+//         .then((response) => response.text())
+//         .then((data) => {
+//           // Handle the user data received from your backend
+//           console.log("User data:", data);
+//           // Update your UI or store the user info as needed
+//         })
+//         .catch((error) => {
+//           console.error("Error:", error);
+//         });
+//     }
 
-  return (
-    <div>
-        {code}
-    </div>
-  )
-}
+//   return (
+//     <div>
+//         {code}
+//     </div>
+//   )
+// }
 
  
 
-export default Callback;
+// export default Callback;
